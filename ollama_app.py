250406502c4320cd6adc41da9fa95c7358c5961a
ssampathkumar104 <83206197+ssampathkumar104@users.noreply.github.com>
2026-02-17 22:17:22 +0530

    first commit

---
+++ ollama_app.py
@@ -0,0 +1,57 @@
+# import openai
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+# from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize OpenAI API client
+# os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGCHAIN_TRACING"] = "true"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+# create the prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an helpful assistant for chatbot."),
+        ("human", "{question}"),
+    ]
+)
+
+def generate_response(question, llm):
+    
+    llm = Ollama(model=llm)
+
+    output_Parser = StrOutputParser()
+    chain = prompt | llm | output_Parser
+    answer = chain.invoke({
+        "question": question
+    })
+    return answer
+
+## title of the app
+st.title("Q&A Chatbot with OpenAI")
+
+# dropdown to select various openAI models
+llm = st.sidebar.selectbox("Select OpenAI Model", ["gemma3:1b ", "gemma:2b","llama2:latest"])
+
+# Adjust response parameters
+# temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
+# max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=200)
+
+## input box for user question
+st.write("Enter your question:")
+
+user_input = st.text_area("You:")
+
+if user_input:
+    response = generate_response(user_input, llm)
+    st.write(response)
+else:
+    st.write("Please enter a question to get a response.")
